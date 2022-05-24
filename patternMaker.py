24fbb07d321540e27930f97c4c198da104a58701
kristina28 <kristina.buss@asu.edu>
2022-05-24 07:20:13 -0700

    Update patternMaker.py

--- patternMaker.py
+++ patternMaker.py
@@ -4,7 +4,6 @@ def patternMaker(pattern):
     pArray = []
     count = 2
     while count <= len(pattern):
-        print(count)
         pLen = len(pattern)
         seq = reverse_complement(pattern)
         seq1 = seq + seq[0:count]
