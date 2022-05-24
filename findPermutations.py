e97d6518684156d23004e8f92697321b019556cc
kristina28 <kristina.buss@asu.edu>
2022-05-24 07:20:24 -0700

    Update findPermutations.py

--- findPermutations.py
+++ findPermutations.py
@@ -13,18 +13,8 @@ def findPermutations(genome,permutation,len1,len2,char1,char2,prefix,count):
             while permutation.lower() in seq[start:end].lower():
                 index = seq.lower().find(permutation.lower(),start,end)
                 subseq = seq[(index-len1):(index+length+len2)]
-                if ((seq[index-1].lower() != char1.lower()) and (seq[index+length].lower() != char2.lower())):
-                    newRecord = [(prefix + "_" + f'{count:04d}'),
-                                 ("[-" + char1.lower() + "]" + permutation + "[-" + char2.lower() + "]"),
-                                 (seq[index-1].upper() + permutation + seq[index+length].upper()),
-                                 str(len(permutation)),
-                                 title.split(" ")[0],
-                                 (str(index-len1) + "-" + str(index+length+len2) + "(+)"),
-                                 ("L=" + str(length + len1 + len2)),
-                                 str(index-len2),
-                                 str(index+length+len1),
-                                 "+",
-                                 subseq]
+                if (((index == 0) or (seq[index-1].lower() != char1.lower())) and (((index+length == len(seq)) or seq[index+length].lower() != char2.lower()))):
+                    newRecord = makeForRecord(prefix, count, char1, permutation, char2, seq, index, length, title, len1, len2, subseq)
                     permArray.append(newRecord)
                     count += 1
                 start = index+length
@@ -35,19 +25,53 @@ def findPermutations(genome,permutation,len1,len2,char1,char2,prefix,count):
             while permRev.lower() in seq[start:end].lower():
                 index = seq.lower().find(permRev.lower(),start,end)
                 subseq = seq[(index-len2):(index+length+len1)]
-                if ((seq[index-1].lower() != char1Rev.lower()) and (seq[index+length].lower() != char2Rev.lower())):
-                    newRecord = [(prefix + "_" + f'{count:04d}'),
-                                 ("[-" + char1.lower() + "]" + permutation + "[-" + char2.lower() + "]"),
-                                 (reverse_complement(seq[index+length].upper()) + permutation + reverse_complement(seq[index-1].upper())),
-                                 str(len(permutation)),
-                                 title.split(" ")[0],
-                                 (str(index-len2) + "-" + str(index+length+len1) + "(-)"),
-                                 ("L=" + str(length + len1 + len2)),
-                                 str(index-len2),
-                                 str(index+length+len1),
-                                 "-",
-                                 reverse_complement(subseq)]
+                if (((index == 0) or (seq[index-1].lower() != char1Rev.lower())) and ((index+length == len(seq)) or (seq[index+length].lower() != char2Rev.lower()))):
+                    newRecord = makeRevRecord(prefix, count, char1, permutation, char2, seq, index, length, title, len1, len2, subseq)
                     permArray.append(newRecord)
                     count += 1
                 start = index+length
     return([permArray,count])
+
+def makeRevRecord(prefix, count, char1, permutation, char2, seq, index, length, title, len1, len2, subseq):
+    if index == 0:
+        edge1 = "N"
+    else:
+        edge1 = reverse_complement(seq[index-1].upper())
+    if index + length >= len(seq):
+        edge2 = "N"
+    else:
+        edge2 = reverse_complement(seq[index+length].upper())
+    newRecord = [(prefix + "_" + f'{count:04d}'),
+                 ("[-" + char1.lower() + "]" + permutation + "[-" + char2.lower() + "]"),
+                 (edge2 + permutation + edge1),
+                 str(len(permutation)),
+                 title.split(" ")[0],
+                 (str(index-len2) + "-" + str(index+length+len1) + "(-)"),
+                 ("L=" + str(len(subseq))),
+                 str(index-len2),
+                 str(index+length+len1),
+                 "-",
+                 reverse_complement(subseq)]
+    return(newRecord)
+
+def makeForRecord(prefix, count, char1, permutation, char2, seq, index, length, title, len1, len2, subseq):
+    if index == 0:
+        edge1 = "N"
+    else:
+        edge1 = seq[index-1].upper()
+    if index + length >= len(seq):
+        edge2 = "N"
+    else:
+        edge2 = seq[index+length].upper()
+    newRecord = [(prefix + "_" + f'{count:04d}'),
+                 ("[-" + char1.lower() + "]" + permutation + "[-" + char2.lower() + "]"),
+                 (edge1 + permutation + edge2),
+                 str(len(permutation)),
+                 title.split(" ")[0],
+                 (str(index-len1) + "-" + str(index+length+len2) + "(+)"),
+                 ("L=" + str(len(subseq))),
+                 str(index-len2),
+                 str(index+length+len1),
+                 "+",
+                 subseq]
+    return(newRecord)
